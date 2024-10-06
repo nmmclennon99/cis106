3bf4fb3d88260608a49320589b2f7e09a0200238
nmmclennon99 <nmmclennon@students.pccc.edu>
2024-10-06 14:10:34 -0400

    lab3 completed

---
+++ challenge_lab.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Author: Natalie Mclennon
+# Date:  10/06/24
+# Description: challenge question solution
+
+echo "this is a place holder"
+echo "you are supposed to do the challenge question alone"
+echo "I can however provide clarifications"
+uname -n
+echo "This is the current user:"
+echo $USER
+echo "This is the current date"
+date +"%A, %B %d %R %Z"
